c4ed4bd787dd8ffeab112ab126e1ec9386f72bfb
Ryan Pritchard <ryan.pritchard@shaed.ai>
2025-11-22 11:23:24 -0600

    Fix case-sensitive import paths for Vercel build

--- src/config/vehicleConfig.ts
+++ src/config/vehicleConfig.ts
@@ -7,10 +7,10 @@ import streetrodData from '@/data/streetrodData.json';
 import morganWhData from '@/data/morganWhData.json';
 import peterbilt520evData from '@/data/peterbilt520ev.json';
 
-import { sections as mullenOneSections } from '@/app/mullenone/constants/sections';
+import { sections as mullenOneSections } from '@/app/mullenOne/constants/sections';
 import { sections as promasterSections } from '@/app/promaster/constants/sections';
 import { sections as streetrodSections } from '@/app/streetrod/constants/sections';
-import { sections as morganWhSections } from '@/app/morganworkhorse/constants/sections';
+import { sections as morganWhSections } from '@/app/morganWorkhorse/constants/sections';
 import { sections as peterbilt520evSections } from '@/app/520ev/constants/sections';
 
 export const vehicleConfigs: { [key: string]: VehicleConfig } = {
